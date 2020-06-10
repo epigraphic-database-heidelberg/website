022ba5c561b3053c798654d5b544915fd57fbc12
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2020-06-10 10:55:11 +0200

    added route for http status code 500 page

--- edh_web_application/__init__.py
+++ edh_web_application/__init__.py
@@ -63,6 +63,14 @@ def create_app(test_config=None):
         return render_template('404.html'), 404
 
     #
+    # 500 page
+    #
+    @app.errorhandler(500)
+    def internal_error(error):
+        return render_template('500.html'), 500
+
+
+    #
     # i18n
     #
     @babel.localeselector
