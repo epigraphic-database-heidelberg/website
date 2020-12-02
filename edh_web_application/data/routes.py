8c9733732c4de9836845f63e89534f0f87eb4ae1
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2020-12-02 16:48:54 +0100

    added data API page

--- edh_web_application/data/routes.py
+++ edh_web_application/data/routes.py
@@ -7,3 +7,8 @@ from flask_babel import _
 @bp_data.route('/data')
 def data():
     return render_template('data/index.html', title=_("Data"), subtitle=_("Open Data Repository"))
+
+
+@bp_data.route('/data/api')
+def api():
+    return render_template('data/api.html', title=_("Data"), subtitle=_("Application Programming Interface (API)"))
