98cc584fb6a0cf0dbb3d7f7668778ad2d1853e80
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2021-04-12 09:13:56 +0200

    improved display on mobiles

--- config.py
+++ config.py
@@ -12,6 +12,12 @@ class Config(object):
 
 
 class DevelopmentConfig(Config):
-    SOLR_BASE_URL = 'http://147.142.112.176:8983/solr/'
-    BASE_HREF = "/test/"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    LANGUAGES = ['en', 'de']
+    BABEL_DEFAULT_LOCALE = 'en'
+    SOLR_BASE_URL = 'http://localhost:8983/solr/'
+    BASE_HREF = "/"
+    HOST = 'https://edh-www.adw.uni-heidelberg.de'
+    JSON_AS_ASCII = False
+    FLASK_DEBUG=1
 
