fba19e1e2e5d6468d828d918c6773584e43a4896
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2021-06-23 15:26:28 +0200

    download data dumps page

--- edh_web_application/data/routes.py
+++ edh_web_application/data/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory, send_file
 
 from . import bp_data
 from flask_babel import _
@@ -12,3 +12,15 @@ def data():
 @bp_data.route('/data/api', strict_slashes=False)
 def api():
     return render_template('data/api.html', title=_("Data"), subtitle=_("Application Programming Interface (API)"))
+
+
+@bp_data.route('/data/download/<filename>', strict_slashes=False)
+def download_file(filename):
+    print(filename)
+    return send_from_directory('data/files/',filename,
+                            as_attachment=True)
+
+
+@bp_data.route('/data/download', strict_slashes=False)
+def download():
+    return render_template('data/download.html', title=_("Data"), subtitle=_("Download Data Dumps"))
